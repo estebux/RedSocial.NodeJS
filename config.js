d8464e1b51b7484d6c276849306e7d0bf4f77d59
Esteban <joelpardo9@gmail.com>
2024-09-11 00:33:38 -0300

    Actual database: MySQL

--- config.js
+++ config.js
@@ -4,5 +4,11 @@ module.exports = {
     },
     jwt: {
         secret: process.env.JWT_SECRET || 'notasecret!',
+    },
+    mysql: {
+        host: process.env.MYSQL_HOST || '127.0.0.1',
+        user: process.env.MYSQL_USER || 'test',
+        password: process.env.MYSQL_PASS || 'qkVzBu]/EiYVkQZz',
+        database: process.env.MYSQL_DB || 'diario',
     }
 }
---
+++ store/mysql.js
@@ -0,0 +1,51 @@
+const mysql = require('mysql');
+const config = require('../config');
+
+const dbconfig = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+}
+
+let connection;
+
+function handleCon(){
+    connection = mysql.createConnection(dbconfig);
+    connection.connect((err) => {
+        if(err){
+            console.error('[db error]', err);
+            setTimeout(handleCon, 2000);
+        }else{
+            console.log('DB connected...');
+        }
+      
+    });
+
+    connection.on('error', err => {
+        console.error('[db error]', err);
+
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            handleCon();
+        }else{
+            throw err;
+        }
+
+    });
+}
+
+handleCon();
+
+function list(table, id){
+    return new Promise((resolve, reject) => {
+        connection.query(`SELECT * FROM ${table}`, (err, data) => {
+            if(err) return reject(err);
+            resolve(data);
+        });
+      
+    });
+}
+
+module.exports = {
+    list, 
+};
